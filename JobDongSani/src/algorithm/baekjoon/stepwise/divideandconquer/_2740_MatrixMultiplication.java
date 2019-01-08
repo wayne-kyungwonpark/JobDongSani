1f50f5307e15803729a6c5110f1cea9a0da2049f
ParkKyungWon <win198978@gmail.com>
2019-01-08 19:52:53 +0900

    행렬 곱셈 소스 수정

--- JobDongSani/src/algorithm/baekjoon/stepwise/divideandconquer/_2740_MatrixMultiplication.java
+++ JobDongSani/src/algorithm/baekjoon/stepwise/divideandconquer/_2740_MatrixMultiplication.java
@@ -6,7 +6,7 @@ import java.io.*;
  * https://github.com/ParkKyungWon/JobDongSani/blob/master/JobDongSani/src/algorithm/baekjoon/stepwise/divideandconquer/_2740_MatrixMultiplication.java
  *
  * Baekjoon > 문제 > 단계별로 풀어보기 > 분할 정복 > 행렬 곱셈
- * 1. n: 행, n: 열, A: n x m 행렬, B: m x n 행렬
+ * 1. A: n x m 행렬, B: m x k 행렬
  * 2. A, B에 값을 저장한 후, 계산해준다.
  * 3. 결과값은 result 에 저장된다.
  */
@@ -15,7 +15,7 @@ public class _2740_MatrixMultiplication {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String str;
-        int n = 0, m = 0;
+        int n = 0, m = 0, k = 0;
         int nFreq = 0, mFreq = 0;
         int[][] A = null, B = null;
         int caseNum = 0;
@@ -25,11 +25,13 @@ public class _2740_MatrixMultiplication {
                 n = Integer.parseInt(strArr[0]);
                 m = Integer.parseInt(strArr[1]);
                 A = new int[n][m];
-                B = new int[m][n];
                 caseNum = 1;
             } else if (nFreq == n && caseNum == 1) {
                 nFreq = 0;
                 mFreq = 0;
+                String[] strArr = str.split(" ");
+                k = Integer.parseInt(strArr[1]);
+                B = new int[m][k];
                 caseNum = 2;
             }else if(caseNum == 1){
                 String[] strArr = str.split(" ");
@@ -39,7 +41,7 @@ public class _2740_MatrixMultiplication {
                 nFreq++;
             }else{
                 String[] strArr = str.split(" ");
-                for (int i = 0; i < n; i++) {
+                for (int i = 0; i < k; i++) {
                     B[mFreq][i] = Integer.parseInt(strArr[i]);
                 }
                 mFreq++;
@@ -49,16 +51,16 @@ public class _2740_MatrixMultiplication {
             }
         }
 
-        int[][] result = new int[n][n];
+        int[][] result = new int[n][k];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < m; k++) {
-                    result[i][j] += (A[i][k] * B[k][j]);
+            for (int j = 0; j < k; j++) {
+                for (int l = 0; l < m; l++) {
+                    result[i][j] += (A[i][l] * B[l][j]);
                 }
             }
         }
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < k; j++) {
                 bw.write(String.valueOf(result[i][j]) + " ");
             }
             if(i != n - 1){
