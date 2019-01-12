832489d7b6d6169a9c951fe07e90583cf429971f
ParkKyungWon <win198978@gmail.com>
2019-01-12 23:38:46 +0900

    mac test commit

--- JobDongSani/src/test/http/StringByteNumTest.java
+++
@@ -1,9 +0,0 @@
-package test.http;
-
-public class StringByteNumTest {
-
-	public static void main(String[] args) {
-		String msg = "SMS Test SMS Test SMS Test SMS Test SMS Test SMS Test SMS Test SMS Test SMS Test";
-		System.out.println(msg.getBytes().length);
-	}
-}
