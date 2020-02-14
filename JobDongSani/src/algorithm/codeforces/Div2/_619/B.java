43228a023978470aa3a53e64bf72b499db3d26c7
ParkKyungWon <win198978@gmail.com>
2020-02-14 21:57:52 +0900

    codeforces div2 #618 문제풀이소스 커밋 - B

--- JobDongSani/src/algorithm/codeforces/Div2/_619/B.java
+++ JobDongSani/src/algorithm/codeforces/Div2/_619/B.java
@@ -31,68 +31,39 @@ public class B {
         for (int i = 0; i < strArr.length; i++) {
             arr[i] = Integer.parseInt(strArr[i]);
         }
-        List<Integer> kCandidates = new ArrayList<>();
-        int prevNonMinusOne = -1;
-        // k가 될 수 있느 후보군을 모음
-        for (int i = 0; i < strArr.length; i++) {
-            if(i == 0){
-                if(arr[i] != -1){
-                    prevNonMinusOne = arr[i];
+
+        int minValue = Integer.MAX_VALUE;
+        int maxValue = Integer.MIN_VALUE;
+        int maxAbsDiff = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == -1){
+                if(i > 0 && arr[i - 1] != -1){
+                    minValue = Math.min(minValue, arr[i - 1]);
+                    maxValue = Math.max(maxValue, arr[i - 1]);
                 }
-            }else if(i == strArr.length - 1){
-                if(prevNonMinusOne == -1){
-                    if(arr[i] == -1){
-                        kCandidates.add(0);
-                    }else{
-                        kCandidates.add(arr[i]);
-                    }
+                while(i < arr.length && arr[i] == -1){
+                    i++;
+                }
+                if(i < arr.length){
+                    minValue = Math.min(minValue, arr[i]);
+                    maxValue = Math.max(maxValue, arr[i]);
                 }else{
-                    if(arr[i] == -1){
-                        kCandidates.add(prevNonMinusOne);
-                    }else{
-                        int sum = prevNonMinusOne + arr[i];
-                        if(sum % 2 != 0){
-                            kCandidates.add(sum / 2 + 1);
-                        }
-                        kCandidates.add(sum / 2);
-                    }
+                    break;
                 }
             }else{
-                if(arr[i] != -1){
-                    if(prevNonMinusOne == -1){
-                        prevNonMinusOne = arr[i];
-                        kCandidates.add(arr[i]);
-                    }else{
-                        int sum = prevNonMinusOne + arr[i];
-                        if(sum % 2 != 0){
-                            kCandidates.add(sum / 2 + 1);
-                        }
-                        kCandidates.add(sum / 2);
-                        prevNonMinusOne = arr[i];
-                    }
+                if(i > 0 && arr[i - 1] != -1){
+                    maxAbsDiff = Math.max(maxAbsDiff, Math.abs(arr[i] - arr[i - 1]));
                 }
             }
         }
 
-        // k후보군 별로 maxAbsDiff를 찾으며, 그 중 min 값을 뽑는다.
-        int minMaxAbsDiff = Integer.MAX_VALUE;
-        int k = 0;
-        for (int i = 0; i < kCandidates.size(); i++) {
-            int kCandidate = kCandidates.get(i);
-            int maxAbsDiff = -1;
-            for (int j = 1; j < strArr.length; j++) {
-                int tmp1 = (arr[j] == -1)? kCandidate : arr[j];
-                int tmp2 = (arr[j - 1] == -1)? kCandidate : arr[j - 1];
-                int absDiff = Math.abs(tmp1 - tmp2);
-                if(absDiff > maxAbsDiff){
-                    maxAbsDiff = absDiff;
-                }
-            }
-            if(minMaxAbsDiff > maxAbsDiff){
-                minMaxAbsDiff = maxAbsDiff;
-                k = kCandidate;
-            }
+        if(minValue == Integer.MAX_VALUE){
+            return "0 0";
         }
-        return minMaxAbsDiff + " " + k;
+
+        int absDiffCandidate = (int) Math.ceil((maxValue - minValue) * 0.5);
+        int minMaxAbsDiff = Math.max(maxAbsDiff, absDiffCandidate);
+
+        return minMaxAbsDiff + " " + (minValue + absDiffCandidate);
     }
 }
