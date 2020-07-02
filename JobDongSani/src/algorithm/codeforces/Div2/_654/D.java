722cbd6ef4769f38094dff2d6364f83962b94674
ParkKyungWon <win198978@gmail.com>
2020-07-02 11:22:58 +0900

    [코드포스 > div2 > 654] 문제풀이소스 커밋 - D

--- JobDongSani/src/algorithm/codeforces/Div2/_654/D.java
+++ JobDongSani/src/algorithm/codeforces/Div2/_654/D.java
@@ -12,40 +12,39 @@ public class D {
             String[] strArr = br.readLine().split(" ");
             int n = Integer.parseInt(strArr[0]);
             int k = Integer.parseInt(strArr[1]);
-            int val = k / 4;
-            int mod = k % 4;
+            int val = k / n;
+            int mod = k % n;
             if(mod == 0){
                 sb.append(0).append("\n");
             }else{
-                val++;
                 sb.append(2).append("\n");
             }
-            int index = 0;
             for (int j = 0; j < n; j++) {
-                if(index < n){
-                    for (int l = 0; l < index; l++) {
+                int add = (mod != 0)? (mod > j)? 1 : 0 : 0;
+                if(j + val < n) {
+                    for (int l = 0; l < j; l++) {
                         sb.append(0);
                     }
-                    for (int l = index; l < val; l++) {
+                    for (int l = j; l < j + val + add; l++) {
                         sb.append(1);
                     }
-                    for (int l = 0; l < n - val; l++) {
+                    for (int l = 0; l < n - (j + val + add); l++) {
                         sb.append(0);
                     }
-                }else{
-                    int left = index % n;
-                    for (int l = 0; l < index; l++) {
+                }
+                else{
+                    int left = (j + val + add) % n;
+                    for (int l = 0; l < left; l++) {
                         sb.append(1);
                     }
-                    for (int l = index; l < val + 1; l++) {
+                    for (int l = left; l < j; l++) {
                         sb.append(0);
                     }
-                    for (int l = 0; l < n - (val + 1); l++) {
+                    for (int l = j; l < n; l++) {
                         sb.append(1);
                     }
                 }
                 sb.append("\n");
-                index++;
             }
         }
         bw.write(sb.toString());
