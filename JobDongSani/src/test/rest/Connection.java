f52e2408edbee535bdcd208fe357310c95c03228
ParkKyungWon <win198978@gmail.com>
2020-09-25 01:21:29 +0900

    rest 수정

--- JobDongSani/src/test/rest/Connection.java
+++ JobDongSani/src/test/rest/Connection.java
@@ -1,5 +1,6 @@
 package test.rest;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.*;
@@ -61,7 +62,52 @@ public class Connection {
         return responseJson;
     }
 
-    public JSONObject postTest(){
-        return null;
+    public JSONObject postTest(String resource, String accept, JSONArray commandArrays){
+
+        HttpURLConnection conn = null;
+        JSONObject responseJson = null;
+
+        URL url = null;
+        try {
+            url = new URL("https://httpbin.org" + resource);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", accept);
+            conn.setDoOutput(true);
+
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+            JSONObject commands = new JSONObject();
+            commands.put("commands", commandArrays);
+            bw.write(commands.toString());
+            bw.flush();
+            bw.close();
+
+            int responseCode = conn.getResponseCode();
+            if (responseCode == 400) {
+                System.out.println("400:: 해당 명령을 실행할 수 없음 (실행할 수 없는 상태일 때, 엘리베이터 수와 Command 수가 일치하지 않을 때, 엘리베이터 정원을 초과하여 태울 때)");
+            } else if (responseCode == 401) {
+                System.out.println("401:: X-Auth-Token Header가 잘못됨");
+            } else if (responseCode == 500) {
+                System.out.println("500:: 서버 에러, 문의 필요");
+            } else { // 성공
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+                String line = "";
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                responseJson = new JSONObject(sb.toString());
+
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return responseJson;
     }
 }
--- JobDongSani/src/test/rest/Main.java
+++ JobDongSani/src/test/rest/Main.java
@@ -1,24 +1,33 @@
 package test.rest;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+//import org.json.
 
 public class Main{
     public static void main(String[] args) {
         Connection con = Connection.getInstance();
-        JSONObject jsonObject = con.getTest("/get", "application/json");
-        toJson(jsonObject, 0);
-        System.out.println(jsonObject);
-        JSONObject jsonObject2 = con.getTest("/response-headers?freeform=", "application/json");
-        toJson(jsonObject2, 0);
-        System.out.println(jsonObject2);
+//        JSONObject jsonObject = con.getTest("/get", "application/json");
+//        changeEasierToSee(jsonObject, 0);
+//        System.out.println(jsonObject);
+//        JSONObject jsonObject2 = con.getTest("/response-headers?freeform=", "application/json");
+//        changeEasierToSee(jsonObject2, 0);
+//        System.out.println(jsonObject2);
+//        JSONObject jsonObject3 = con.postTest("/post", "application/json", new JSONArray());
+//        changeEasierToSee(jsonObject3, 0);
+//        System.out.println(jsonObject3);
+        JSONObject jsonObject4 = con.postTest("/post", "application/json", new JSONArray());
+        JSONObject headers = jsonObject4.getJSONObject("headers");
+        changeEasierToSee(headers, 0);
+        System.out.println(jsonObject4);
     }
 
-    private static void toJson(JSONObject jsonObject, int level){
+    private static void changeEasierToSee(JSONObject jsonObject, int level){
         for(String key : jsonObject.keySet()){
             Object value = jsonObject.get(key);
             if(value instanceof JSONObject){
                 System.out.println(key + ": ");
-                toJson((JSONObject) value, level + 1);
+                changeEasierToSee((JSONObject) value, level + 1);
             }else{
                 for (int i = 0; i < level; i++) {
                     System.out.print("    ");
